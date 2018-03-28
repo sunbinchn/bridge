2827cfbf64063b70cd571bd693bb22feb6d2a7eb
sunbinchn <sunbinchn@foxmail.com>
2018-03-28 23:26:35 +0800

    monitor point

--- src/main/java/com/bridge/entity/MonitorPoint.java
+++ src/main/java/com/bridge/entity/MonitorPoint.java
@@ -3,6 +3,8 @@ package com.bridge.entity;
 public class MonitorPoint {
     private Integer id;
     private String name;
+    private User user;
+    private Area area;
 
     public Integer getId() {
         return id;
@@ -18,5 +20,21 @@ public class MonitorPoint {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
     }
 }
