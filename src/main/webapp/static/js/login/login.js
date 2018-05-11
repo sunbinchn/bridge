f592e5c736595fca41bdf9d37fc1ecc5d7aad73f
sunbinchn <sunbinchn@foxmail.com>
2018-05-11 14:24:00 +0800

    enhance

--- src/main/webapp/static/js/login/login.js
+++ src/main/webapp/static/js/login/login.js
@@ -14,7 +14,7 @@ $(function () {
             data: data,
             success: function (result) {
                 if (result.success) {
-                    window.location.href = '/bridge/home/index.html'
+                    window.location.href = '/bridge/userManage/getAll'
                 } else {
                     $('.form-signin-heading').text(result.message);
                 }
